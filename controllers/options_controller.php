e14d07f5a37c4b7955e019690e0f47c1edc413d8
Fredrik Sundström <fredrik.sundstrom@gmail.com>
2012-06-25 10:35:28 +0200

    Added capabilitites

--- controllers/options_controller.php
+++ controllers/options_controller.php
@@ -6,6 +6,32 @@ namespace RolesRemake
   {
     public function index()
     {
+      global $roles_and_capabilities;
+
+      $current_blog_id = get_current_blog_id();
+
+      $blog = \WpMvc\Blog::find( $current_blog_id );
+
+      $roles_and_capabilities = unserialize( $blog->options->{"wp_{$blog->blog_id}_user_roles"}->option_value );
+
+      if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+        if ( isset( $_POST['capabilities']['switch_themes'] ) ) {
+          $roles_and_capabilities['editor']['capabilities']['switch_themes'] = true;
+        } else {
+          $roles_and_capabilities['editor']['capabilities']['switch_themes'] = false;
+        }
+
+        if ( isset( $_POST['capabilities']['edit_theme_options'] ) ) {
+          $roles_and_capabilities['editor']['capabilities']['edit_theme_options'] = true;
+        } else {
+          $roles_and_capabilities['editor']['capabilities']['edit_theme_options'] = false;
+        }
+
+        $blog->options->{"wp_{$blog->blog_id}_user_roles"}->option_value = serialize( $roles_and_capabilities );
+
+        $blog->save();
+      }
+
       $this->render( $this, "index" );
     }
   }
--- views/options/index.php
+++ views/options/index.php
@@ -1,34 +1,24 @@
-<?php global $site; global $theme_names; ?>
+<?php global $roles_and_capabilities; ?>
 
 <div class="wrap">
   <div id="icon-options-general" class="icon32"><br></div>
-  <h2><?php _e( 'Roles Remake Options' ); ?></h2>
-  <?php
-    global $wp_roles;
-
-    echo wp_hash_password( 'rattmuff11?' );
-
-    $id = 69;
-
-    $blog = \WpMvc\Blog::find( $id );
-
-
-    \WpMvc\DevHelper::dump( $blog );
-
-
-    $roles_and_capabilities = unserialize( $blog->options->{"wp_{$id}_user_roles"}->option_value );
-
-    \WpMvc\DevHelper::dump( $roles_and_capabilities );
-
-    $roles_and_capabilities['editor']['capabilities']['switch_themes'] = true;
-    $roles_and_capabilities['editor']['capabilities']['edit_themes'] = false;
-    $roles_and_capabilities['editor']['capabilities']['update_themes'] = false;
-    $roles_and_capabilities['editor']['capabilities']['delete_themes'] = false;
-    $roles_and_capabilities['editor']['capabilities']['install_themes'] = false;
-    $roles_and_capabilities['editor']['capabilities']['edit_theme_options'] = true;
-
-    $blog->options->{"wp_{$id}_user_roles"}->option_value = serialize( $roles_and_capabilities );
-
-    $blog->save();
-  ?>
+  <h2><?php _e( 'Editor Capabilities' ); ?></h2>
+  <form action="/tarthumlan/wp-admin/users.php?page=editor_capabilities_options_menu" method="post">
+    <table class="form-table">
+      <tbody>
+        <tr valign="top">
+          <th scope="row"><?php _e( 'Capabilities' ); ?></th>
+          <td>
+            <input name="capabilities[switch_themes]" type="checkbox" id="capabilities_switch_themes" <?php echo $roles_and_capabilities['editor']['capabilities']['switch_themes'] ? "checked='checked'" : ''; ?>>
+            <label for="capabilities_switch_themes"><?php _e( 'Switch Themes' ); ?></label><br />
+            <input name="capabilities[edit_theme_options]" type="checkbox" id="capabilities_edit_theme_options" <?php echo $roles_and_capabilities['editor']['capabilities']['edit_theme_options'] ? "checked='checked'" : ''; ?>>
+            <label for="capabilities_edit_theme_options"><?php _e( 'Edit Theme Options' ); ?></label><br />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p class="submit">
+      <input type="submit" name="submit" id="submit" class="button-primary" value="Spara ändringar">
+    </p>
+  </form>
 </div>
